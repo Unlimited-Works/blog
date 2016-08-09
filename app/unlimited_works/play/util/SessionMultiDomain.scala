cc355c0cc41e9026b154b83c04446d2d26283a85
LoranceChen <wangchen@qiaobutang.com>
2016-08-09 23:37:24 +0800

    session use redis

--- app/unlimited_works/play/util/SessionMultiDomain.scala
+++ app/unlimited_works/play/util/SessionMultiDomain.scala
@@ -1,60 +1,80 @@
 package unlimited_works.play.util
 
+import com.redis.RedisClient
 import play.api.mvc.Request
 import unlimited_works.play.controllers.util.Config
-
-import scala.collection.mutable
 
 /**
   * should be save a specify place,such as a memcache server
   */
 object SessionMultiDomain {
   //out key is session id, inner key is every data's id
-  private val sessions = mutable.Map[String, mutable.Map[String, String]]()
+//  private val sessions = mutable.Map[String, mutable.Map[String, String]]()
+  var redisClient = new RedisClient("127.0.0.1", 6379)
 
   def puts(id1: String, data: Map[String, String]): Unit = {
-    sessions.synchronized{
-      sessions.get(id1) match {
-        case Some(theSession) => theSession ++= data
-        case None => sessions.+=(id1 -> (mutable.Map[String, String]() ++= data))
-      }
-    }
+    redisClient.hmset(id1, data)
+//    redisClient.h()
+//    client.
+//    sessions.synchronized{
+//      sessions.get(id1) match {
+//        case Some(theSession) => theSession ++= data
+//        case None => sessions.+=(id1 -> (mutable.Map[String, String]() ++= data))
+//      }
+//    }
   }
 
   def put(id1: String, data: (String, String)): Unit = {
-    sessions.synchronized{
-      sessions.get(id1) match {
-        case Some(theSession) => theSession += data
-        case None => sessions.+=(id1 -> (mutable.Map[String, String]() += data))
-      }
-    }
+    redisClient.hset(id1, data._1, data._2)
+
+//    sessions.synchronized{
+//      sessions.get(id1) match {
+//        case Some(theSession) => theSession += data
+//        case None => sessions.+=(id1 -> (mutable.Map[String, String]() += data))
+//      }
+//    }
   }
 
   def removes(id1: String, id2: List[String]): Unit = {
-    sessions.synchronized(sessions.get(id1).foreach{theS =>
-      id2.foreach(theS -= _)
-    })
+    redisClient.hdel(id1, id2)
+
+//    sessions.synchronized(sessions.get(id1).foreach{theS =>
+//      id2.foreach(theS -= _)
+//    })
   }
 
   def removeAll(id1: String): Unit = {
-    sessions.synchronized(sessions.remove(id1))
+    redisClient.del(id1)
+
+//    sessions.synchronized(sessions.remove(id1))
   }
 
   def remove(id1: String, id2: String): Unit = {
-    sessions.synchronized(sessions.get(id1).map(_.remove(id2)))
+    redisClient.hdel(id1, id2)
+
+//    sessions.synchronized(sessions.get(id1).map(_.remove(id2)))
   }
 
   def gets(id1: String, id2: List[String]) = {
-    sessions.synchronized{
-      sessions.get(id1).map{ theS =>
-        theS.filter{x => id2.contains(x._1)}.toMap
-      }.getOrElse(Map[String, String]())
+    redisClient.hgetall(id1).map{x =>
+      x.filter(y => id2.contains(y._1))
     }
+//    sessions.synchronized{
+//      sessions.get(id1).map{ theS =>
+//        theS.filter{x => id2.contains(x._1)}.toMap
+//      }.getOrElse(Map[String, String]())
+//    }
   }
 
-  def getAll(id1: String) = sessions.synchronized(sessions.get(id1))
+  def getAll(id1: String) = {
+    redisClient.hgetall(id1)
+//    sessions.synchronized(sessions.get(id1))
+  }
 
-  def get(id1: String, id2: String) = sessions.synchronized(sessions.get(id1).flatMap(_.get(id2)))
+  def get(id1: String, id2: String) = {
+    redisClient.hget(id1, id2)
+//    sessions.synchronized(sessions.get(id1).flatMap(_.get(id2)))
+  }
 
   def getAccountId[A](request: Request[A]) = {
     request.cookies.get(Config.CookieSession.GOD_SESSION).map(_.value).flatMap{
